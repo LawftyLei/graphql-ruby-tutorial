a110831b70f1106ea4a4aa76eb9643a3d8ce8ee9
LawftyLei <lei@lawfty.com>
2024-02-26 10:56:52 -0500

    sign in, posted by user

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,5 +3,6 @@ module Types
     field :create_link, mutation: Mutations::CreateLink
     field :create_user, mutation: Mutations::CreateUser
     field :create_link, mutation: Mutations::CreateLink
+    field :sign_in_user, mutation: Mutations::SignInUser
   end
 end
